0b6fa8d8f1a1c2e455643f7c87e2c97dea6df728
Emily Smith <emsm2434@colorado.edu>
2024-11-06 10:43:42 -0700

    Emily: Database now functional

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,24 +1,27 @@
+-- need to make PostgresSQL not mySQL
 CREATE TABLE users (
-    user_id SERIAL NOT NULL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
     password VARCHAR(30) NOT NULL,
     timer TIME DEFAULT '00:00:00',
-    progress TINYINT DEFAULT '0'
+    progress INT DEFAULT '0'
 );
 
 CREATE TABLE puzzles (
-    puzzle_id SERIAL NOT NULL PRIMARY KEY,
-    value TINYINT NOT NULL
+    puzzle_id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    value INT NOT NULL
 );
 
 CREATE TABLE users_puzzles (
-    user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
-    puzzle_id FOREIGN KEY (puzzle_id) REFERENCES puzzles(puzzle_id),
-    is_solved BOOL DEFAULT '0',
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    puzzle_id INT REFERENCES puzzles(puzzle_id) ON DELETE CASCADE,
+    is_solved BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (user_id, puzzle_id)
 );
 
 CREATE TABLE items (
     item_id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
     status VARCHAR(10) CHECK(status IN('unknown','found','active','disabled'))
 );
--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -3,43 +3,32 @@
 
 -- 1-3 users with username and passwords, default timer and progress
 INSERT INTO users (username, password)
-VALUES (
+VALUES
     ('test', '123'),
-    ('admin', 'admin')
-    ('beep', 'boop')
-);
+    ('admin', 'admin'),
+    ('beep', 'boop');
 
--- puzzles 1-4 all valued at 25
-INSERT INTO puzzles (value)
-VALUES (
-    (25),
-    (25),
-    (25),
-    (25)
-);
+-- puzzles 1-3, values at 33,33, and 34 (total 100)
+INSERT INTO puzzles (name, value)
+VALUES
+    ('Lock_and_key',34),
+    ('Light_up_tree',33),
+    ('Open_the_present',33);
 
--- users 1-3 with puzzles 1-4 all unsolved
+-- users 1-3 with puzzles 1-3 all unsolved
 INSERT INTO users_puzzles (user_id, puzzle_id)
-VALUES (
+VALUES
     (1,1),
     (1,2),
     (1,3),
-    (1,4),
     (2,1),
     (2,2),
     (2,3),
-    (2,4),
     (3,1),
     (3,2),
-    (3,3),
-    (3,4)
-);
+    (3,3);
 
--- items 1-4 all unknown
-INSERT INTO items (status)
-VALUES (
-    ('unknown'),
-    ('unknown'),
-    ('unknown'),
-    ('unknown')
-);
+-- items 1 set to unknown
+INSERT INTO items (name,status)
+VALUES
+    ('key','unknown');
