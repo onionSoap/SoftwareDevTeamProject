5064ecbf6e5214730709fa768da60268f24c5f04
Emily Smith <emsm2434@colorado.edu>
2024-11-18 16:50:39 -0700

    Emily: Updated DB to include users_items, added all items to DB initial generation, register populates users_puzzles and users_items now, and all tests for update_item_status and update_is_solved passed.

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -10,6 +10,7 @@ VALUES
     ('beep','$2a$10$n4si2D1TNuaFohFTsxx/A.HAlDRag2cs6fkwmU2XdYiAYLGmmXTOi');
 
 -- puzzles 1-3, values at 33,33, and 34 (total 100)
+-- update with Sofia's puzzles
 INSERT INTO puzzles (name, value)
 VALUES
     ('Lock_and_key',34),
@@ -25,11 +26,52 @@ VALUES
     (2,1),
     (2,2),
     (2,3);
-    -- (3,1),
-    -- (3,2),
-    -- (3,3);
 
 -- items 1 set to unknown
-INSERT INTO items (name,status)
+-- update with all items Sofia sent one adding ONE item works :(
+-- ["carrot","key","cookie_cutter","flour","sugar","butter","antlers","mistletoe","lucky_star","christmas_lights","wreath"];
+INSERT INTO items (name)
 VALUES
-    ('key','unknown');
+    ('key'), --don't forget to change this to , instead of ;
+    ('carrot'),
+    ('cookie_cutter'),
+    ('flour'),
+    ('sugar'),
+    ('butter'),
+    ('antlers'),
+    ('mistletoe'),
+    ('lucky_star'),
+    ('christmas_lights'),
+    ('wreath');
+
+-- CREATE TABLE users_items (
+--     user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+--     item_id INT REFERENCES puzzles(puzzle_id) ON DELETE CASCADE,
+--     status VARCHAR(10) CHECK(status IN('unknown','found','active','disabled')) DEFAULT 'unknown',
+--     PRIMARY KEY (user_id, item_id)
+-- );
+--only one item rn, two users populated by default
+INSERT INTO users_items (user_id, item_id)
+VALUES
+    (1,1),
+    (1,2),
+    (1,3),
+    (1,4),
+    (1,5),
+    (1,6),
+    (1,7),
+    (1,8),
+    (1,9),
+    (1,10),
+    (1,11),
+    (2,1),
+    (2,2),
+    (2,3),
+    (2,4),
+    (2,5),
+    (2,6),
+    (2,7),
+    (2,8),
+    (2,9),
+    (2,10),
+    (2,11);
