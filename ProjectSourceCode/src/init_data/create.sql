47d3b3330fb07be25cc4dbac99b78332100758e6
emsm2434 <90489000+emsm2434@users.noreply.github.com>
2024-11-21 16:32:04 -0700

    Emily: Updated db to have scenes and scene_status

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -3,8 +3,10 @@ DROP TABLE IF EXISTS users_puzzles;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS puzzles;
 DROP TABLE IF EXISTS items;
-
-
+DROP TABLE IF EXISTS users_items;
+DROP TABLE IF EXISTS scenes;
+DROP TABLE IF EXISTS scene_state;
+-- need to make PostgresSQL not mySQL
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(30) NOT NULL UNIQUE,
@@ -12,7 +14,7 @@ CREATE TABLE users (
     timer TIME DEFAULT '00:00:00',
     progress INT DEFAULT '0'
 );
-
+-- removing these puzzle tables and changing the game to be "item status based" vs "puzzle status based" maybe...
 CREATE TABLE puzzles (
     puzzle_id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
@@ -28,6 +30,23 @@ CREATE TABLE users_puzzles (
 
 CREATE TABLE items (
     item_id SERIAL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    status VARCHAR(10) CHECK(status IN('unknown','found','active','disabled'))
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE users_items (
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    item_id INT REFERENCES items(item_id) ON DELETE CASCADE,
+    status VARCHAR(10) CHECK(status IN('unknown','found','active','disabled')) DEFAULT 'unknown',
+    PRIMARY KEY (user_id, item_id)
+);
+
+CREATE TABLE scenes (
+    scene_id VARCHAR(2) PRIMARY KEY
+);
+
+CREATE TABLE scene_state (
+    scene_state_id SERIAL PRIMARY KEY,
+    scene_number VARCHAR(2) REFERENCES scenes(scene_id) ON DELETE CASCADE,
+    object VARCHAR(30) NOT NULL,
+    visible_state VARCHAR(7) CHECK(visible_state IN('visible','hidden'))
 );
